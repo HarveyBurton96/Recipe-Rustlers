8a3b68b947e59f295f296af44669cfe69406f628
HarveyBurton96 <harvey.burton@sky.com>
2023-01-26 15:44:45 +0000

    Add admin superuser and models for Post and Comments

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title']
+    summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('recipe', 'body', 'name', 'created_on')
+    list_filter = ('created_on', 'name')
+    search_fields = ['name', 'email', 'body']
