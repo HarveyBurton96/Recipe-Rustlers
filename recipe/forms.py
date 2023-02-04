5a1aeadaa81588ac6047876cb8ddf172a38af6b3
HarveyBurton96 <harvey.burton@sky.com>
2023-02-04 17:30:41 +0000

    Add widgets for form

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 from .models import Post
 
+CHOICES = [(1, 'Draft'), (2, 'Published')]
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'featured_image', 'status', 'surving')
+        fields = ('title','slug', 'featured_image', 'author', 'status', 'surving')
+        
+        status = forms.ChoiceField(choices=CHOICES)
+        author = User
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
+            'surving': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
--- recipe/views.py
+++ recipe/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic, View
 from .models import Post
 from .forms import PostForm
-
+from django.http import HttpResponseRedirect
 
 
 
@@ -14,4 +14,15 @@ class PostList(generic.ListView):
 
 
 def add_recipe(request):
-    return render(request, 'add_a_recipe.html', {})
+    submitted = False
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_a_recipe?submitted=True')
+    else:
+        form = PostForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'add_a_recipe.html', {'form': form, 'submitted': submitted})
