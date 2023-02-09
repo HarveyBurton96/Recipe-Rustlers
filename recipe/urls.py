c18bb69e0399486d788e65267452572ab69c85ce
HarveyBurton96 <harvey.burton@sky.com>
2023-02-09 14:41:32 +0000

    Create recipe deatil view

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,10 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+
+app_name = "recipe" 
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name="recipe_detail"),
     path("add_a_recipe", views.add_recipe, name="add_recipe"),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 from .forms import PostForm
 from django.http import HttpResponseRedirect
-
 
 
 class PostList(generic.ListView):
@@ -26,3 +25,24 @@ def add_recipe(request):
             submitted = True
 
     return render(request, 'add_a_recipe.html', {'form': form, 'submitted': submitted})
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
