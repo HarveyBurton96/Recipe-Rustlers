25da7b8b25ef47a0076a7b86ea3b92816ed91c8f
HarveyBurton96 <harvey.burton@sky.com>
2023-02-12 12:00:59 +0000

    Add abillity to comment on recipes

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 CHOICES = [(1, 'Draft'), (2, 'Published')]
+
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ('title','slug', 'featured_image', 'author', 'status', 'surving')
-        
+        fields = ('title', 'slug', 'featured_image', 'author', 'status', 'surving',)
+
         status = forms.ChoiceField(choices=CHOICES)
         author = User
 
@@ -19,3 +20,9 @@ class PostForm(ModelForm):
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
             'surving': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.http import HttpResponseRedirect
 
 
@@ -59,8 +59,45 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
                 "ingredients": ingredients,
                 "instructions": instructions,
-                "liked": liked
+                "liked": liked,
+                "comment_form": CommentForm(),
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by('created_on')
+        ingredients = post.ingredients
+        instructions = post.instructions
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "ingredients": ingredients,
+                "instructions": instructions,
+                "liked": liked,
+                "comment_form": CommentForm(),
             },
         )
