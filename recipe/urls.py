cac01c7b8e6f886c926ae020467dd1c5b6d7fc3c
HarveyBurton96 <harvey.burton@sky.com>
2023-02-26 14:31:19 +0000

    Fixed indentation and made the ' consistent for urls

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,23 +4,99 @@ from django.urls import path, include
 app_name = "recipe"
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="home"),
-    path('<slug:slug>/', views.PostDetail.as_view(), name="recipe_detail"),
-    path('add_a_recipe_detail/<slug:slug>', views.add_recipeDetails.as_view(), name="add_a_recipe_detail"),
-    path("popular_recipe", views.PopularRecipes.as_view(), name="popular_recipe"),
-    path("new_recipe", views.NewRecipes.as_view(), name="new_recipe"),
-    path("add_a_recipe", views.add_recipe, name="add_recipe"),
-    path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
-    path("loved_recipes",  views.LovedRecipes.as_view(), name="loved_recipes"),
-    path("search_recipe",  views.SearchRecipe, name="search_recipe"),
-    path('update_recipe/<slug:slug>', views.UpdateRecipe, name="update_recipe"),
-    path('update_instructions/<id>', views.UpdateInstructions, name="update_instructions"),
-    path('update_ingredients/<id>', views.UpdateIngredient, name="update_ingredients"),
-    path('update_comment/<id>', views.UpdateComment, name="update_comment"),
-    path("your_recipes",  views.YourRecipes.as_view(), name="your_recipes"),
-    path('your_recipe/<slug:slug>/', views.YourPostDetail.as_view(), name="your_recipe_detail"),
-    path('delete_comment/<id>', views.deleteComment, name="delete_comment"),
-    path('delete_recipe/<slug:slug>', views.deleteRecipe, name="delete_recipe"),
-    path('delete_instruction/<id>', views.deleteInstruction, name="delete_instruction"),
-    path('delete_ingredient/<id>', views.deleteIngredient, name="delete_ingredient"),
+    path(
+        "",
+        views.PostList.as_view(),
+        name="home"
+        ),
+    path(
+        "<slug:slug>/",
+        views.PostDetail.as_view(),
+        name="recipe_detail"
+        ),
+    path(
+        "add_a_recipe_detail/<slug:slug>",
+        views.add_recipeDetails.as_view(),
+        name="add_a_recipe_detail"
+        ),
+    path(
+        "popular_recipe",
+        views.PopularRecipes.as_view(),
+        name="popular_recipe"
+        ),
+    path(
+        "new_recipe",
+        views.NewRecipes.as_view(),
+        name="new_recipe"
+        ),
+    path(
+        "add_a_recipe",
+        views.add_recipe,
+        name="add_recipe"
+        ),
+    path(
+        "like/<slug:slug>",
+        views.RecipeLike.as_view(),
+        name="recipe_like"
+        ),
+    path(
+        "loved_recipes",
+        views.LovedRecipes.as_view(),
+        name="loved_recipes"
+        ),
+    path(
+        "search_recipe",
+        views.SearchRecipe,
+        name="search_recipe"
+        ),
+    path(
+        "update_recipe/<slug:slug>",
+        views.UpdateRecipe,
+        name="update_recipe"
+        ),
+    path(
+        "update_instructions/<id>",
+        views.UpdateInstructions,
+        name="update_instructions"
+        ),
+    path(
+        "update_ingredients/<id>",
+        views.UpdateIngredient,
+        name="update_ingredients"
+        ),
+    path(
+        "update_comment/<id>",
+        views.UpdateComment,
+        name="update_comment"
+        ),
+    path(
+        "your_recipes",
+        views.YourRecipes.as_view(),
+        name="your_recipes"
+        ),
+    path(
+        "your_recipe/<slug:slug>/",
+        views.YourPostDetail.as_view(),
+        name="your_recipe_detail"
+        ),
+    path(
+        "delete_comment/<id>",
+        views.deleteComment,
+        name="delete_comment"
+        ),
+    path(
+        "delete_recipe/<slug:slug>",
+        views.deleteRecipe,
+        name="delete_recipe"
+        ),
+    path(
+        "delete_instruction/<id>",
+        views.deleteInstruction,
+        name="delete_instruction"
+        ),
+    path(
+        "delete_ingredient/<id>",
+        views.deleteIngredient,
+        name="delete_ingredient"
+        ),
 ]
